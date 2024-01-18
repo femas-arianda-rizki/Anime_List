1d0944c55c79afc9ca3fc8937f05856496266958
femas arianda <femasariandarizki@students.undip.ac.id>
2024-01-18 23:58:09 +0700

    commit all changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
-import Image from 'next/image'
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>ANIMELIST</h1>
+//     </div>
+//   )
+// }
+import AnimeList from "@/app/components/AnimeList"
 
-export default function Home() {
+const home = () => {
+
+  
+
   return (
     <div>
-      <h1>ANIMELIST</h1>
+      <h1>Paling Populer</h1>
+      <AnimeList/>
     </div>
   )
 }
+
+export default home
---
+++ src/app/search/page.js
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h1>SEARCH PAGE</h1>
+    </div>
+  )
+}
+
+export default page

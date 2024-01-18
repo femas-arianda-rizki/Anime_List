0cb64af8c6cc7a1ff4b6e23fe004b5b9133b4284
femas arianda <femasariandarizki@students.undip.ac.id>
2024-01-19 00:56:21 +0700

    commit all changes

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -1,22 +1,10 @@
 import Image from "next/image";
 
-const AnimeList = () => {
+const AnimeList = ({ title, images }) => {
   return (
-    <div className="grid grid-cols-3 gap-2 h-32">
-      <div className="bg-indigo-500">
-        <Image
-          src="https://placehold.co/600x400/png"
-          alt="..."
-          width={600}
-          height={600}
-        />
-        <h3>Judul Anime</h3>
-      </div>
-      <div className="bg-indigo-500">KOTAK 2</div>
-      <div className="bg-indigo-500">KOTAK 3</div>
-      <div className="bg-indigo-500">KOTAK 4</div>
-      <div className="bg-indigo-500">KOTAK 5</div>
-      <div className="bg-indigo-500">KOTAK 6</div>
+    <div className="">
+      <Image src={images} alt="..." width={350} height={350} />
+      <h3 className="font-bold md:text-xl text-md p-4">{title}</h3>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -5,18 +5,31 @@
 //     </div>
 //   )
 // }
-import AnimeList from "@/app/components/AnimeList"
+import AnimeList from "@/app/components/AnimeList";
 
-const home = () => {
-
-  
+const Home = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`
+  );
+  const anime = await response.json();
 
   return (
     <div>
       <h1>Paling Populer</h1>
-      <AnimeList/>
+      <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4">
+        {anime.data.map((data) => {
+          return (
+            <div key={data.mal_id} className="shadow-xl">
+              <AnimeList
+                title={data.title}
+                images={data.images.webp.image_url}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default home
+export default Home;

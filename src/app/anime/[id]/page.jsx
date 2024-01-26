488e77c2b9f33a2ad0c79595c4257ec8c49558c6
femas arianda <femasariandarizki@students.undip.ac.id>
2024-01-26 23:58:27 +0700

    changes

---
+++ src/app/anime/[id]/page.jsx
@@ -0,0 +1,41 @@
+import { getAnimeResponse } from "@/app/libs/api-libs";
+import Image from "next/image";
+
+const Page = async ({ params: { id } }) => {
+  const anime = await getAnimeResponse(`anime/${id}`);
+  console.log(anime);
+
+  return (
+    <>
+      <div className="pt-4 px-4">
+        <h3 className="text-2xl text-color-primary">
+          {anime.data.title} - {anime.data.year}
+        </h3>
+      </div>
+
+      <div className="pt-4 px-4 flex gap-2 text-color-primary">
+        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
+          <h3>Peringkat</h3>
+          <p>{anime.data.rank} </p>
+        </div>
+        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2>
+          <h3>Skor</h3>
+          <p>{anime.data.score} </p>
+        </div>
+      </div>
+
+      <div className="pt-4 px-4 flex sm:flex-nowrap flex-wrap gap-2 text-color-primary">
+        <Image
+          src={anime.data.images.webp.image_url}
+          alt={anime.data.images.jpg.image_url}
+          width={250}
+          height={250}
+          className="w-full rounded object-cover"
+        />
+        <p className="text-justify text-xl">{anime.data.synopsis} </p>
+      </div>
+    </>
+  );
+};
+
+export default Page;

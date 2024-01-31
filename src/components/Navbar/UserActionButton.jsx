8ee17a0b514bf46c751bf140c98f0e5c2d87663b
femas arianda <femasariandarizki@students.undip.ac.id>
2024-01-31 23:58:52 +0700

    commit

--- src/components/Navbar/UserActionButton.jsx
+++ src/components/Navbar/UserActionButton.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import { authUserSession } from "../../libs/auth-libs";
 
-// const UserActionButton = async () => {
-//   const user = await authUserSession();
+const UserActionButton = async () => {
+  const user = await authUserSession();
 
-//   const actionLabel = user ? "Sign Out" : "Sign In";
-//   const actionURL = user ? "/api/auth/signout" : "/api/auth/signin";
+  const actionLabel = user ? "Sign Out" : "Sign In";
+  const actionURL = user ? "/api/auth/signout" : "/api/auth/signin";
 
   return (
     <div className="flex justify-between gap-2">

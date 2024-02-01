1aa65b2c9fd05250e2ed5eca3602e5fa47b50466
femas arianda <femasariandarizki@students.undip.ac.id>
2024-02-01 17:32:00 +0700

    koneksi database MySQL dengan Prisma ORM

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -1,59 +1,69 @@
 import { getAnimeResponse } from "../../../libs/api-libs";
 import VideoPlayer from "../../../components/Utilities/VideoPlayer";
 import Image from "next/image";
+import CollectionButton from "../../../components/AnimeList/CollectionButton";
+import { authUserSession } from "../../../libs/auth-libs";
+import prisma from "../../../libs/prisma";
 
 const Page = async ({ params: { id } }) => {
   const anime = await getAnimeResponse(`anime/${id}`);
-  console.log(anime);
+  const user = await authUserSession();
+  const collection = await prisma.collection.findFirst({
+    where: { user_email: user?.email, anime_mal_id: id },
+  });
+  console.log({ collection });
 
   return (
     <>
       <div className="pt-4 px-4">
         <h3 className="text-2xl text-color-primary">
-          {anime.data.title} - {anime.data.year}
+          {anime.data?.title} - {anime.data?.year}
         </h3>
+        {!collection && user && (
+          <CollectionButton anime_mal_id={id} user_email={user?.email} />
+        )}
       </div>
 
       <div className="pt-4 px-4 flex gap-2 text-color-primary overflow-x-auto">
         <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
           <h3>Peringkat</h3>
-          <p>{anime.data.rank} </p>
+          <p>{anime.data?.rank} </p>
         </div>
         <div
           className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2"
           S
         >
           <h3>Skor</h3>
-          <p>{anime.data.score} </p>
+          <p>{anime.data?.score} </p>
         </div>
         <div
           className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2"
           S
         >
           <h3>Anggota</h3>
-          <p>{anime.data.members} </p>
+          <p>{anime.data?.members} </p>
         </div>
         <div
           className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2"
           S
         >
           <h3>Episode</h3>
-          <p>{anime.data.episodes} </p>
+          <p>{anime.data?.episodes} </p>
         </div>
       </div>
 
       <div className="pt-4 px-4 flex sm:flex-nowrap flex-wrap gap-2 text-color-primary">
         <Image
-          src={anime.data.images.webp.image_url}
-          alt={anime.data.images.jpg.image_url}
+          src={anime.data?.images.webp.image_url}
+          alt={anime.data?.images.jpg.image_url}
           width={250}
           height={250}
           className="w-full rounded object-cover"
         />
-        <p className="text-justify text-xl">{anime.data.synopsis} </p>
+        <p className="text-justify text-xl">{anime.data?.synopsis} </p>
       </div>
       <div>
-        <VideoPlayer youtubeId={anime.data.trailer.youtube_id} />
+        <VideoPlayer youtubeId={anime.data?.trailer.youtube_id} />
       </div>
     </>
   );
